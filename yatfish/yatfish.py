060dda860b64bf87bd5f29f359264282a3ace9d9
bleuzkernel <bleuzkernel@gmail.com>
2023-04-10 20:15:53 -0600

    Docker volumnes fixed, pyATS dynamic test, AETest loop integrated, Additional Batfish questions added & Code refactored.

--- yatfish/yatfish.py
+++ yatfish/yatfish.py
@@ -2,6 +2,7 @@ import os
 import json
 import logging
 import pandas as pd
+
 from pyats import aetest
 from pyats.log.utils import banner
 from pybatfish.client.session import Session
@@ -17,40 +18,61 @@ log = logging.getLogger(__name__)
 
 # Batfish Variables
 SNAPSHOT_PATH = "./snapshot"
-output_dir = "./output"
+OUTPUT_DIR = "./output"
+
 
 # ----------------
 # AE Test Setup
 # ----------------
 class common_setup(aetest.CommonSetup):
     """Common Setup section"""
-# ----------------
-# Connected to devices
-# ----------------
+
+    # ----------------
+    # Connected to devices
+    # ----------------
     @aetest.subsection
     def connect_to_devices(self, testbed):
         """Connect to all the devices"""
         testbed.connect()
-# ----------------
-# Mark the loop for Input Discards
-# ----------------
+
+    # ----------------
+    # Mark the loop for Input Discards
+    # ----------------
     @aetest.subsection
     def loop_mark(self, testbed):
         aetest.loop.mark(Yatfish, device_name=testbed.devices)
+
 
 # ----------------
 # Test Case #1
 # ----------------
 class Yatfish(aetest.Testcase):
-    """Parse all pyATS learn config and learn interfaces data"""
+    """pyATS and Batfish integration."""
+
+    def save_result(self, output, file_name):
+        output_file = os.path.join(OUTPUT_DIR, f"{self.device.alias}_{file_name}")
+        output.to_csv(f"{output_file}.csv")
+        output.to_json(f"{output_file}.json", indent=4)
+        output.to_html(f"{output_file}.html")
+
+    def run_batfish_question(self, question, file_name):
+        try:
+            if hasattr(self.bf.q, question) and callable(
+                func := getattr(self.bf.q, question)
+            ):
+                result = func().answer().frame()
+                self.save_result(result, file_name)
+        except Exception as error:
+            log.error(
+                f"Error running Batfish question {question} on device {self.device.alias}:\n{error}"
+            )
 
     @aetest.test
     def setup(self, testbed, device_name):
-        """ Testcase Setup section """
+        """Testcase Setup section"""
         # connect to device
         self.device = testbed.devices[device_name]
-        # Loop over devices in tested for testing
-    
+
     @aetest.test
     def get_running_config(self):
         self.raw_running_config = self.device.execute("show run")
@@ -59,7 +81,6 @@ class Yatfish(aetest.Testcase):
     def save_running_config(self):
         with open(f"{ SNAPSHOT_PATH }/configs/{ self.device.alias }.cfg", "w") as fh:
             fh.write(self.raw_running_config)
-            fh.close()
 
     @aetest.test
     def establish_session(self):
@@ -68,102 +89,41 @@ class Yatfish(aetest.Testcase):
     @aetest.test
     def init_snapshot(self):
         self.bf.init_snapshot(SNAPSHOT_PATH, overwrite=True)
-    
-    @aetest.test
-    def node_properties(self):
-        self.node_result = self.bf.q.nodeProperties().answer().frame()
 
     @aetest.test
-    def save_node_results(self):
-        self.node_result.to_csv(f"{ output_dir }/{ self.device.alias }_node_properties.csv")
-        self.node_result.to_json(f"{ output_dir }/{ self.device.alias }_node_properties.json", indent=4)
-        self.node_result.to_html(f"{ output_dir }/{ self.device.alias }_node_properties.html")
+    def load_batfish_questions(self):
+        questions = [
+            {"question": "nodeProperties", "file_name": "node_properties"},
+            {"question": "interfaceProperties", "file_name": "interface_properties"},
+            {"question": "bgpProcessConfiguration", "file_name": "bgp_process_configuration"},
+            {"question": "bgpPeerConfiguration", "file_name": "bgp_peer_configuration"},
+            {"question": "hsrpProperties", "file_name": "hsrp_properties"},
+            {"question": "ospfProcessConfiguration", "file_name": "ospf_process_configuration"},
+            {"question": "ospfAreaConfiguration", "file_name": "ospf_area_configuration"},
+            {"question": "ospfInterfaceConfiguration", "file_name": "ospf_interface_configuration"},
+            {"question": "mlagProperties", "file_name": "mlag_properties"},
+            {"question": "namedStructures", "file_name": "named_structures"},
+            {"question": "definedStructures", "file_name": "defined_structures"},
+            {"question": "referencedStructures", "file_name": "referenced_structures"},
+            {"question": "undefinedReferences", "file_name": "undefined_references"},
+            {"question": "unusedStructures", "file_name": "unused_structures"},
+            {"question": "switchedVlanProperties", "file_name": "switched_vlan_properties"},
+            {"question": "vrrpProperties", "file_name": "vrrp_properties"},
+        ]
+        aetest.loop.mark(self.ask_batfish_question, question=questions)
 
     @aetest.test
-    def interface_properties(self):
-        self.interface_result = self.bf.q.interfaceProperties().answer().frame()
+    def ask_batfish_question(self, section, question):
+        section.uid = question["question"]
+        self.run_batfish_question(question["question"], question["file_name"])
 
-    @aetest.test
-    def save_interface_results(self):
-        self.interface_result.to_csv(f"{ output_dir }/{ self.device.alias }_interface_properties.csv")
-        self.interface_result.to_json(f"{ output_dir }/{ self.device.alias }_interface_properties.json", indent=4)
-        self.interface_result.to_html(f"{ output_dir }/{ self.device.alias }_interface_properties.html")
-
-    @aetest.test
-    def undefined_references(self):
-        self.undefined_result = self.bf.q.undefinedReferences().answer().frame()
-
-    @aetest.test
-    def save_undefined_results(self):
-        self.undefined_result.to_csv(f"{ output_dir }/{ self.device.alias }_undefined_results.csv")
-        self.undefined_result.to_json(f"{ output_dir }/{ self.device.alias }_undefined_results.json", indent=4)
-        self.undefined_result.to_html(f"{ output_dir }/{ self.device.alias }_undefined_results.html")
-
-    @aetest.test
-    def unused_structures(self):
-        self.unused_result = self.bf.q.unusedStructures().answer().frame()
-
-    @aetest.test
-    def save_unused_structures(self):
-        self.unused_result.to_csv(f"{ output_dir }/{ self.device.alias }_unused_structures.csv")
-        self.unused_result.to_json(f"{ output_dir }/{ self.device.alias }_unused_structures.json", indent=4)
-        self.unused_result.to_html(f"{ output_dir }/{ self.device.alias }_unused_structures.html")
-
-    @aetest.test
-    def hsrpProperties(self):
-        self.hsrp_result = self.bf.q.hsrpProperties().answer().frame()
-
-    @aetest.test
-    def save_hsrp_result(self):
-        self.hsrp_result.to_csv(f"{ output_dir }/{ self.device.alias }_hsrp_result.csv")
-        self.hsrp_result.to_json(f"{ output_dir }/{ self.device.alias }_hsrp_result.json", indent=4)
-        self.hsrp_result.to_html(f"{ output_dir }/{ self.device.alias }_hsrp_result.html")
-
-    @aetest.test
-    def ospfProcessConfiguration(self):
-        self.ospfProcess_result = self.bf.q.ospfProcessConfiguration().answer().frame()
-
-    @aetest.test
-    def save_ospfProcess_result(self):
-        self.ospfProcess_result.to_csv(f"{ output_dir }/{ self.device.alias }_ospfProcess_result.csv")
-        self.ospfProcess_result.to_json(f"{ output_dir }/{ self.device.alias }_ospfProcess_result.json", indent=4)
-        self.ospfProcess_result.to_html(f"{ output_dir }/{ self.device.alias }_ospfProcess_result.html")
-
-    @aetest.test
-    def ospfInterfaceConfiguration(self):
-        self.ospfInterface_result = self.bf.q.ospfInterfaceConfiguration().answer().frame()
-
-    @aetest.test
-    def save_ospfInterface_result(self):
-        self.ospfInterface_result.to_csv(f"{ output_dir }/{ self.device.alias }_ospfInterface_result.csv")
-        self.ospfInterface_result.to_json(f"{ output_dir }/{ self.device.alias }_ospfInterface_result.json", indent=4)
-        self.ospfInterface_result.to_html(f"{ output_dir }/{ self.device.alias }_ospfInterface_result.html")
-
-    @aetest.test
-    def ospfAreaConfiguration(self):
-        self.ospfArea_result = self.bf.q.ospfAreaConfiguration().answer().frame()
-
-    @aetest.test
-    def save_ospfArea_result(self):
-        self.ospfArea_result.to_csv(f"{ output_dir }/{ self.device.alias }_ospfArea_result.csv")
-        self.ospfArea_result.to_json(f"{ output_dir }/{ self.device.alias }_ospfArea_result.json", indent=4)
-        self.ospfArea_result.to_html(f"{ output_dir }/{ self.device.alias }_ospfArea_result.html")
-
-    @aetest.test
-    def switchedVlanProperties(self):
-        self.switchedVlanProperties_result = self.bf.q.switchedVlanProperties().answer().frame()
-
-    @aetest.test
-    def save_switchedVlanProperties_result(self):
-        self.switchedVlanProperties_result.to_csv(f"{ output_dir }/{ self.device.alias }_switchedVlanProperties_result.csv")
-        self.switchedVlanProperties_result.to_json(f"{ output_dir }/{ self.device.alias }_switchedVlanProperties_result.json", indent=4)
-        self.switchedVlanProperties_result.to_html(f"{ output_dir }/{ self.device.alias }_switchedVlanProperties_result.html")
 
 class CommonCleanup(aetest.CommonCleanup):
     @aetest.subsection
     def disconnect_from_devices(self, testbed):
         testbed.disconnect()
 
+
 # for running as its own executable
-if __name__ == '__main__':
+if __name__ == "__main__":
     aetest.main()
